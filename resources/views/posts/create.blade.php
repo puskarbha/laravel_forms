c179c01c12ae6f6be3d1fdb0c5887354ee48aabb
Puskar <puskarbha@gmail.com>
2022-11-09 23:07:39 +0545

    starting

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <form method="post" action="/post">
+        <input type="text" name="title" placeholder="Enter title">
+        <input type="submit" name="submit">
+    </form>
+
+    @yield('footer')
